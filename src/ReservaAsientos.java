1b69ac01ca8e792f961163b2074880c152fcb2cd
Veronica <veronica.marcela.bar@gmail.com.ar>
2023-10-07 18:35:47 -0300

    ejercicios simples

---
+++ src/ReservaAsientos.java
@@ -0,0 +1,34 @@
+/*
+import java.util.Scanner;
+
+public class ReservaAsientos {
+    public static void main(String[] args) {
+        char asientos[][] = new char[10][10];
+        boolean flag = false;
+        Scanner sc = new Scanner(System.in);
+        int fila, asiento;
+
+        for (int f= 0; f< 10; f++) {
+            for (int c = 0; c < 10; c++) {
+                asiento[f][c] = 'L';
+            }
+        }
+
+        System.out.println("------------- ASIENTOS DISPONIBLES -------------");
+
+        while(flag!=true){
+            System.out.println("Ingrese la Fila y asiento que desea reservar: ");
+            System.out.print("Fila (entre 0 y 9): ");
+            fila = sc.nextInt();
+
+            System.out.print("Asiento (entre 0 y 9): ");
+            asiento = sc.nextInt();
+
+
+
+        }
+
+    }
+}
+*/
+
